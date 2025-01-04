7137120928c75e08cfb7affae005f37eaa6cbf7e
SimonBandiera <simon.bandiera@epitech.eu>
2025-01-04 09:13:14 +0100

    [+] add assert include

--- src/Program.cpp
+++ src/Program.cpp
@@ -6,6 +6,7 @@ Lexer::Program::Program(std::vector<Token> &tokens)
     // TODO make this dynamic to include only the necessary files
     this->includes.emplace_back("types/JsAny.hpp");
     this->includes.emplace_back("class/Console.hpp");
+    this->includes.emplace_back("class/Assert.hpp");
 }
 
 void Lexer::Program::parse()
